8f6eba4d3236d60787e9b5a47946a4fc81a2632b
Andrew Thal <467872+athal7@users.noreply.github.com>
2026-01-02 13:07:42 -0600

    fix: use lodash template syntax for semantic-release config (#52)

--- .releaserc.js
+++ .releaserc.js
@@ -18,26 +18,9 @@ module.exports = {
           {
             path: 'dist/devcontainer-multi.tar.gz',
             label: 'OCDC Release Archive',
-            name: (release) => `ocdc-${release.version}.tar.gz`
+            name: 'ocdc-${nextRelease.version}.tar.gz'
           }
-        ],
-        releaseName: (release) => `OCDC ${release.version}`,
-        releaseBody: (release) => `## 🎉 Release ${release.version}
-
-### 📦 Installation
-\`\`\`bash
-brew install ocdc/tap/ocdc
-\`\`\`
-
-Or download the archive below.
-
-### 📋 Changes
-${release.notes}
-
-### 🔗 Assets
-- Binary release: \`ocdc-${release.version}.tar.gz\`
-- Homebrew formula will be automatically updated
-        `
+        ]
       }
     ]
   ]
